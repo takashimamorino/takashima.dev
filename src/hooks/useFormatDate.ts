8ca98a49a2afbdba0ce8967fba1b94437ed1d7ab
takashimamorino <does1026@gmail.com>
2021-11-27 13:05:30 +0900

    chore: remove

--- src/hooks/useFormatDate.ts
+++
@@ -1,9 +0,0 @@
-import dayjs from 'dayjs'
-
-export const useFormatDate = (
-  date: string,
-  dateOrder: string
-): { formattedDate: string } => {
-  const formattedDate = dayjs(date).format(dateOrder)
-  return { formattedDate }
-}
