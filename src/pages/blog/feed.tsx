e9643db111327730683ebea9e24e0662d7a9e1ac
takashimamorino <does1026@gmail.com>
2021-12-15 21:21:52 +0900

    chore: change

--- src/pages/blog/feed.tsx
+++ src/pages/blog/feed.tsx
@@ -7,7 +7,6 @@ export const getServerSideProps = async ({ res }: GetServerSidePropsContext) => 
   const xml = await generateFeedXml();
 
   res.statusCode = 200;
-  res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate'); // 24時間キャッシュする
   res.setHeader('Content-Type', 'text/xml');
   res.end(xml);
 
